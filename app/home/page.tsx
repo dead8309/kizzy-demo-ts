c7d2851b1fa7ecea4d07c86c2a2a54a17a3aa9ab
Vaibhav <vaibhavmonu8309@gmail.com>
2023-06-08 14:22:23 +0000

    fix: reference error

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,9 +1,12 @@
 "use client"
+
 import { useRouter } from "next/navigation"
 
 export default function Home() {
-    const router = useRouter()
-    let token = localStorage.getItem('token') || ''
-    if (token.length > 0) router.push('/rpc')
-    else router.push('/login')
+  const router = useRouter()
+  if (typeof window !== "undefined") {
+    let token = localStorage.getItem("token") || ""
+    if (token.length > 0) router.push("/rpc")
+    else router.push("/login")
+  }
 }
