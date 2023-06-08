d9088573b20bda802cde94d5ab01ffe25bc46f63
Vaibhav <vaibhavmonu8309@gmail.com>
2023-06-08 17:05:29 +0000

    add toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
   title: {
@@ -46,6 +47,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
               <div className="flex-1">{children}</div>
             </div>
             <TailwindIndicator />
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
